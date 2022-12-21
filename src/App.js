372dd9eb1351aacc09d09486a58de35b9a24b8cb
BrunoFranzon <bruno@madhat.de>
2022-12-21 16:17:18 +0100

    tofolist

--- src/App.js
+++ src/App.js
@@ -1,42 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [inputText, setInputText] = useState("");
 
-  const [inputText, setInputText]  = useState('X');
+  const [todoList, setTodoList] = useState([]);
 
-
+  console.log("todoList: ", todoList);
 
   return (
     <div className="App">
-  
-        <div className='todoInputContainer'> 
-    
-          <input 
-            className='todoInput' 
-            placeholder='add tarefa' 
-            onChange={(event)=> {               
-              setInputText(event.target.value)
-            }}
-          />
+      <div className="todoInputContainer">
+        <input
+          className="todoInput"
+          placeholder="add tarefa"
+          value={inputText}
+          onChange={(event) => {
+            const textoDigitado = event.target.value;
+            setInputText(textoDigitado);
+          }}
+        />
+
+        <button
+          className="addBtn"
+          onClick={() => {
+            setTodoList([...todoList, { id: inputText, text: inputText }]);
+            setInputText("");
+          }}
+        >
+          +
+        </button>
+      </div>
+
+      {/* onde cliquei = 'leite'
+      todoList  = ['leite', 'arroz', 'pao']; */}
+
+      <div className="todoListContainer">
+        {todoList.map((toDo, idx) => {
+           
+          return (
+            <div className="toDo">
+              {toDo.id} --- {toDo.text}
+              <button onClick={() => {
+
+              const filteredToDoList  = todoList?.filter(x => x.id !== toDo.id);
+              console.log('filteredToDoList: ', filteredToDoList);
+              //setTodoList(filteredToDoList)            
+                
+              }}> X </button>
+            </div>
+          );
 
 
-          <button
-           className='addBtn'
-           onClick={()=>{
-  
-             
-
-           }}
-           > 
-          + 
-          </button>        
-
-
-  
-        </div>
-
+        })}
+      </div>
     </div>
   );
 }
